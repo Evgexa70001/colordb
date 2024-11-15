b635df099e1581f62dd35246cf8e0cb058b312cb
Evgexa70001 <58167315+Evgexa70001@users.noreply.github.com>
2024-11-15 18:35:48 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      keyframes: {
+        shimmer: {
+          '100%': { transform: 'translateX(100%)' }
+        }
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
