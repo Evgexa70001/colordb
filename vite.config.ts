4cae4499eb8cfd0e46c2a8350daec6098ae19d92
Evgexa70001 <58167315+Evgexa70001@users.noreply.github.com>
2024-12-14 12:02:20 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['@libsql/client']
-  }
+    exclude: ['@libsql/client'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@contexts': path.resolve(__dirname, './src/contexts'),
+    },
+  },
 });
