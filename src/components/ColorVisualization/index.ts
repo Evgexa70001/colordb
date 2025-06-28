31fb6afe5ab3d9e958df30b9a2dc905c5cde09b0
Evgexa70001 <58167315+Evgexa70001@users.noreply.github.com>
2025-06-28 19:51:32 +0300

    Delete src directory

--- src/components/ColorVisualization/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as ColorMap2D } from './ColorMap2D'
-export { default as ColorVisualizationHub } from './ColorVisualizationHub'
--- src/components/Equipment/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as EquipmentCard } from './EquipmentCard';
-export { default as EquipmentDataCard } from './EquipmentDataCard';
-export { default as EquipmentDetailsModal } from './EquipmentDetailsModal';
-export { default as EquipmentSortControls } from './EquipmentSortControls';
-export { default as NewEquipmentModal } from './NewEquipmentModal';
-export { default as ImagePreviewModal } from './ImagePreviewModal';
-export { default as SkeletonEquipmentCard } from './SkeletonEquipmentCard';
