0f8a1fff3e0cbc795e10916e96154a1b2f428f16
Evgexa70001 <58167315+Evgexa70001@users.noreply.github.com>
2025-07-08 15:08:37 +0300

    Delete src directory

--- src/components/ColorDetails/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as ColorComparison } from './ColorComparison';
-export { default as ColorDetailsModal } from './ColorDetailsModal';
-export { default as ColorInfo } from './ColorInfo';
-export { default as RecipeComparison } from './RecipeComparison';
-export { default as SimilarColors } from './SimilarColors';
